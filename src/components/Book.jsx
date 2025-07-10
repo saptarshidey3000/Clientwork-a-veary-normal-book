53802342905351025b35920df76fd20b75b6cc67
Saptarshi dey <saptarshidey3000@gmail.com>
2025-07-11 01:04:37 +0530

    started adding pictures , check book-page folder , changed the structure little bit

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,31 +1,93 @@
-import React from 'react'
+import React from 'react';
 import HTMLFlipBook from 'react-pageflip';
 
 const Book = () => {
   return (
-    <div className='w-[100vw] flex justify-center h-[100vh] items-center overflow-hidden relative'>
-      <HTMLFlipBook className="absolute inset-0 m-auto"width={500} height={600} maxShadowOpacity={0.5} drawShadow={true} showCover={true} size='fixed'>
-        <div className="demoPage bg-blue-50  border-1 "> <div className='flex justify-center items-center w-[100%] h-[100%]'> Page 1 </div> </div>
-        <div className="demoPage bg-blue-50 border-1"><div className='flex justify-center items-center w-[100%] h-[100%]'>
-          <img
-            src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2JyNzJwOW93NzJrNDkyeWc4Zm50Z3NjcXdnNHVvOTd3eWlnOWM5dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ov9jQX2Ow4bM5xxuM/giphy.gif"
-            alt="Funny animation"
-            className="w-64 h-auto"
-          /> </div></div>
-        <div className="demoPage bg-blue-50 border-1" ><div className='flex justify-center items-center w-[100%] h-[100%]'> <img
-          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZmg5YnFjNzVkOWVtMGp4NWY5ZnptcXU3Mzdndnh3M3VraWFtdGNweSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wQclSvQQKtObPx9UKY/giphy.gif"
-          alt="Funny animation"
-          className="w-64 h-auto"
-        /> </div></div>
-        <div className="demoPage bg-blue-50 border-1"><div className='flex justify-center items-center w-[100%] h-[100%]'> Page 4 </div></div>
-        <div className="demoPage bg-blue-50 border-1" ><div className='flex justify-center items-center w-[100%] h-[100%]'> Page 5 </div></div>
-        <div className="demoPage bg-blue-50 border-1"><div className='flex justify-center items-center w-[100%] h-[100%]'> Page 6 </div></div>
-        <div className="demoPage bg-blue-50 border-1" ><div className='flex justify-center items-center w-[100%] h-[100%]'> Page 7 </div></div>
-        <div className="demoPage bg-blue-50 border-1"><div className='flex justify-center items-center w-[100%] h-[100%]'> Page 8 </div></div>
-      </HTMLFlipBook>
-      
-    </div>
-  )
-}
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center overflow-hidden relative bg-gradient-to-br from-pink-200 via-white to-pink-100">
+      <HTMLFlipBook
+        className="absolute inset-0 m-auto"
+        width={500}
+        height={600}
+        size="fixed"
+        drawShadow={true}
+        maxShadowOpacity={0.5}
+        showCover={true}
+      >
+        {/* Page 1 - img */}
+<div className="demoPage bg-blue-50 border-1">
+  <div className="flex justify-center items-center w-full h-full">
+    <img
+      src="/book-pages/page1.jpg"
+      alt="Page 1"
+      className="w-full h-full object-center"
+      onError={(e) => { e.target.src = "/book-pages/placeholder.jpg"; }}
+    />
+  </div>
+</div>
 
-export default Book
+
+
+        {/* Page 2 - img */}
+<div className="demoPage bg-blue-50 border-1">
+  <div className="flex justify-center items-center w-full h-full">
+    <img
+      src="/book-pages/page2.jpg"
+      alt="Page 2"
+      className="w-full h-full object-center"
+      onError={(e) => { e.target.src = "/book-pages/placeholder.jpg"; }}
+    />
+  </div>
+</div>
+
+        {/* Page 3 - img */}
+  <div className="demoPage bg-blue-50 border-1">
+  <div className="flex justify-center items-center w-full h-full">
+    <img
+      src="/book-pages/page3.jpg"
+      alt="Page 3"
+      className="w-full h-full object-center"
+      onError={(e) => { e.target.src = "/book-pages/placeholder.jpg"; }}
+    />
+  </div>
+</div>
+
+        {/* Page 4 - Text */}
+        <div className="demoPage bg-blue-50 border-1">
+          <div className="flex justify-center items-center w-full h-full">
+            Page 4
+          </div>
+        </div>
+
+        {/* Page 5 - Text */}
+        <div className="demoPage bg-blue-50 border-1">
+          <div className="flex justify-center items-center w-full h-full">
+            Page 5
+          </div>
+        </div>
+
+        {/* Page 6 - Text */}
+        <div className="demoPage bg-blue-50 border-1">
+          <div className="flex justify-center items-center w-full h-full">
+            Page 6
+          </div>
+        </div>
+
+        {/* Page 7 - Text */}
+        <div className="demoPage bg-blue-50 border-1">
+          <div className="flex justify-center items-center w-full h-full">
+            Page 7
+          </div>
+        </div>
+
+        {/* Page 8 - Text */}
+        <div className="demoPage bg-blue-50 border-1">
+          <div className="flex justify-center items-center w-full h-full">
+            Page 8
+          </div>
+        </div>
+      </HTMLFlipBook>
+    </div>
+  );
+};
+
+export default Book;
